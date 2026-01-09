a0e497ac4adc85658afba545f8cc3bc26185fce8
Tosin Ekundayo <tosinekshally@gmail.com>
2026-01-09 21:35:51 +0100

    worked on the user

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,3 +1,3 @@
-// export const API_BASE_URL = 'https://archbrideg-backend.onrender.com/api';
-export const API_BASE_URL = 'http://localhost:45365/api';
+export const API_BASE_URL = 'https://console.craduleapi.com.ng/api';
+// export const API_BASE_URL = 'http://localhost:45365/api';
 
